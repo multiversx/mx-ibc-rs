5094cd1ab2c7f8aaab021d1765e82b8c69827d80
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-09-17 09:41:25 +0300

    rename

--- host/wasm/src/lib.rs
+++ host/wasm/src/lib.rs
@@ -16,7 +16,7 @@ multiversx_sc_wasm_adapter::allocator!();
 multiversx_sc_wasm_adapter::panic_handler!();
 
 multiversx_sc_wasm_adapter::endpoints! {
-    ibc
+    host
     (
         init => init
         upgrade => upgrade
